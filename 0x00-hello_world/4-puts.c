919910ca0a172b5b4164135044a8f1d424f6f656
adroit16 <aderinsayo16@gmail.com>
2022-09-08 12:34:15 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -15,6 +15,6 @@ int main(void)
 
 	/* Tried printing it directly */
 
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle"\);
 	return (0);
 }
