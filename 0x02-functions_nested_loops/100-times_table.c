13c247d24aa6007c41e997d2ed612c47e8a02371
adroit16 <aderinsayo16@gmail.com>
2022-09-15 13:12:23 -0700

    ameendments

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,52 @@
 #include "main.h"
-/**
- * print_times_table - prints times table
- * @n: times table to use
- * Return: void
- */
 
+/**
+ * prints_times_table - prints n times table
+ * @n: the number to be treated
+ *
+ * Return: Number matrix
+ */
 void print_times_table(int n)
 {
-	int a = 0, rep, b;
+	int x, y, z;
 
-	if (n < 0 || n > 15)
-		return;
-
-	while (a <= n)
+	if (n >= 0 && n <= 14)
 	{
-		for (b = 0; b <= n; b++)
+		for (x = 0; x <= n; x++)
 		{
-			rep = a * b;
-			if (b == 0)
-				_putchar('0' + rep);
-			else if (rep < 10)
+			for (y = 0; y <= n; y++)
 			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + rep);
+				z = x * y;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
 			}
-			else if (rep < 100)
-			{
-				_putchar('0' + rep / 100);
-				_putchar('0' + (rep - 100) / 10);
-				_putchar('0' + rep % 10);
-			}
-			if (b < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-		a++;
 	}
 }
-
 
