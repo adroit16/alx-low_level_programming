dca16a90235a6a6fb10759c4b412fc1670a06a81
adroit16 <aderinsayo16@gmail.com>
2022-09-26 07:15:22 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - locate 1sr occurence of char in string and returns pointer there
+ * @s: string to search
+ * @c: target character
+ * Return: pointer to that character in string
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return (NUll);
+}
